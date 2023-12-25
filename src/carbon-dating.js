74b87e5db468cca367d5b41e264d1d27ea3f031a
Zhansaule Telisheva <jansaule97@mail.ru>
2023-12-26 02:30:47 +0500

    feat: add carbon dating task solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (sampleActivity === undefined || typeof sampleActivity !== 'string' || parseInt(sampleActivity) < 0 || isNaN(+sampleActivity) || +sampleActivity === 0 || +sampleActivity > 15) {
+    return false;
+  }
+
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k);
+
+  return t;
 }
 
 module.exports = {
   dateSample
 };
+
+dateSample('-5.8');
